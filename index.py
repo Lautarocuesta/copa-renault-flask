af147e41ca36e31a7566f619050040c428c59eea
unknown <lauticuesta567@gmail.com>
2024-06-06 19:39:02 -0300

    sponsors casi listo

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import random
+import uuid
 
 app = Flask(__name__)
 
-# Configuración de la base de datosssss
+# Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///carrito.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -14,13 +15,18 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     items = db.Column(db.String, nullable=False)
     total_price = db.Column(db.Float, nullable=False)
+    order_number = db.Column(db.String(50), unique=True, nullable=False)
 
 # Inicializar la base de datos dentro del contexto de la aplicación
 with app.app_context():
     db.create_all()
 
-# Lista de sponsors de ejemplo
-sponsors = ["Nike", "Adidas", "Coca-Cola", "Pepsi", "Visa", "Apple", "Samsung"]
+# Lista de sponsors
+sponsors = [
+    "beltran", "city", "congreso", "fie", "grido", 
+    "image", "khalama", "patagonia", "pretty", "principito", 
+    "pritty", "s20",
+]
 
 @app.route('/')
 def home():
@@ -33,13 +39,11 @@ def contacto():
 
 @app.route('/submit_contact', methods=['POST'])
 def submit_contact():
-    # Aquí procesas los datos del formulario
     nombre = request.form['nombre']
     return f'Formulario enviado por {nombre}'
 
 @app.route('/sponsors')
 def sponsors_random():
-    # Mezclar los sponsors en orden aleatorio
     random.shuffle(sponsors)
     return render_template("spo.html", sponsors=sponsors)
 
@@ -90,22 +94,25 @@ def send_cart():
     total_price = sum(item['price'] * item['quantity'] for item in cart.values())
     items = ", ".join([f"{name} x{details['quantity']}" for name, details in cart.items()])
 
-    # Guardar el pedido en la base de datos
-    order = Order(items=items, total_price=total_price)
+    order_number = generate_order_number()
+
+    order = Order(items=items, total_price=total_price, order_number=order_number)
     db.session.add(order)
     db.session.commit()
 
-    # Limpiar el carrito
     session.pop('cart', None)
     session.modified = True
 
-    flash(f'Carrito enviado. Total: ${total_price:.2f}')
+    flash(f'Carrito enviado. Total: ${total_price:.2f} - Número de pedido: {order_number}')
     return redirect(url_for('carta'))
 
 @app.route('/orders')
 def orders():
     orders = Order.query.all()
     return render_template('orders.html', orders=orders)
+
+def generate_order_number():
+    return str(uuid.uuid4().hex)[:10]
 
 if __name__ == '__main__':
     app.run(debug=True)
