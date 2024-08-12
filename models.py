80647e1fcb96746d33557434596fe77e93234512
GabyCostilla <d47722877@alumnos.itr.edu.ar>
2024-08-12 18:57:06 -0300

    mejorando base de datos 2

--- models.py
+++ models.py
@@ -1,14 +1,2 @@
-class ContactMessage(db.Model):
-    __tablename__ = 'ContactMessages'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), nullable=False)
-    school = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    roles = db.Column(db.String(50), nullable=False)
-    other_role = db.Column(db.String(100))
-    age = db.Column(db.Integer, nullable=False)
-    message = db.Column(db.Text, nullable=False)
-    submitted_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<ContactMessage {self.id}>'
+from copa-renault import db  # Asegúrate de que la importación esté correcta según tu estructura
+from datetime import datetime
